4efbfdd141317c6771a8eb6b7ffe99b5bb864f7d
Cassidy <cassidysm@bluelinedesigners.onmicrosoft.com>
2025-02-04 16:59:12 -0600

    init

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Team from "../views/Team.vue";
+import Projects from "../views/Projects.vue";
+
+const routes = [
+  { path: "/", name: "Dashboard", component: Dashboard },
+  { path: "/team", name: "Team", component: Team },
+  { path: "/projects", name: "Projects", component: Projects },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
